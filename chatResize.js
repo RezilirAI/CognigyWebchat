f61be1919ea3b7c93bcaf88055f18982e09a30b7
Chris-RezilirAi <162471552+Chris-RezilirAi@users.noreply.github.com>
2024-04-11 16:47:52 -0400

    Update chatResize.js

--- chatResize.js
+++ chatResize.js
@@ -1,44 +1,42 @@
-function addResize() {
-    var callback = function (mutationsList) {
-        for (var mutation of mutationsList) {
-            if (mutation.type == 'childList') {
-                if (mutation.addedNodes.length > 0) {
-                    let wcObject = mutation.addedNodes[0];
-                    var wcState = "small"
-    
-                    let b = document.createElement("button")
-                    b.id = "myLink";
-                    b.innerText = "Expand chat window";
-                    b.style.border = 'none';
-                    b.style.background = 'transparent';
-                    wcObject.insertBefore(b, null);
-    
-                    b.onclick = function () {
-                        if (wcState == "small") {
-                            wcObject.style.width = "98%";
-                            b.innerText = "Shrink chat window"
-                            wcState = "big"
-                        } else if (wcState = "big") {
-                            wcObject.style.width = '';
-                            b.innerText = "Expand chat window"
-                            wcState = "small"
-                        }
-    
-                        return false;
+var callback = function (mutationsList) {
+    for (var mutation of mutationsList) {
+        if (mutation.type == 'childList') {
+            if (mutation.addedNodes.length > 0) {
+                let wcObject = mutation.addedNodes[0];
+                var wcState = "small"
+
+                let b = document.createElement("button")
+                b.id = "myLink";
+                b.innerText = "Expand chat window";
+                b.style.border = 'none';
+                b.style.background = 'transparent';
+                wcObject.insertBefore(b, null);
+
+                b.onclick = function () {
+                    if (wcState == "small") {
+                        wcObject.style.width = "98%";
+                        b.innerText = "Shrink chat window"
+                        wcState = "big"
+                    } else if (wcState = "big") {
+                        wcObject.style.width = '';
+                        b.innerText = "Expand chat window"
+                        wcState = "small"
                     }
+
+                    return false;
                 }
             }
         }
     }
-    var targetNode = document.getElementsByClassName('webchat-root')[0];
-    console.log(targetNode);
-    var config = {
-        attributes: false,
-        childList: true,
-        subtree: false
-    };
-    
-    var observer = new MutationObserver(callback);
-    
-    observer.observe(targetNode, config);
 }
+var targetNode = document.getElementsByClassName('webchat-root')[0];
+console.log(targetNode);
+var config = {
+    attributes: false,
+    childList: true,
+    subtree: false
+};
+
+var observer = new MutationObserver(callback);
+
+observer.observe(targetNode, config);
