0b57c0dd3fd10ade165b0e3cc6063b98f2d8d765
Chris-RezilirAi <162471552+Chris-RezilirAi@users.noreply.github.com>
2024-04-12 10:58:38 -0400

    Update chatResize.js

--- chatResize.js
+++ chatResize.js
@@ -1,40 +1,41 @@
 var callback = function (mutationsList) {
-    for (var mutation of mutationsList) {
-        if (mutation.type == 'childList') {
-            if (mutation.addedNodes.length > 0) {
-                let wcObject = mutation.addedNodes[0];
-                var wcState = "small"
+  for (var mutation of mutationsList) {
+    if (mutation.type == 'childList') {
+      let wcObject = [...mutation.addedNodes].filter((x) => x.id == 'webchatWindow')[0];
+      if (wcObject) {
+        observer.disconnect();
+        var wcState = "small"
 
-                let b = document.createElement("button")
-                b.id = "myLink";
-                b.innerText = "Expand chat window";
-                b.style.border = 'none';
-                b.style.background = 'transparent';
-                wcObject.insertBefore(b, null);
+        let b = document.createElement("button")
+        b.id = "webchatSizeToggle";
+        b.innerText = "Expand chat window";
+        b.style.border = 'none';
+        b.style.background = 'transparent';
+        wcObject.insertBefore(b, null);
 
-                b.onclick = function () {
-                    if (wcState == "small") {
-                        wcObject.style.width = "98%";
-                        b.innerText = "Shrink chat window"
-                        wcState = "big"
-                    } else if (wcState = "big") {
-                        wcObject.style.width = '';
-                        b.innerText = "Expand chat window"
-                        wcState = "small"
-                    }
+        b.onclick = function () {
+          if (wcState == "small") {
+            wcObject.style.width = "98%";
+            b.innerText = "Shrink chat window"
+            wcState = "big"
+          } else if (wcState = "big") {
+            wcObject.style.width = '';
+            b.innerText = "Expand chat window"
+            wcState = "small"
+          }
 
-                    return false;
-                }
-            }
+          return false;
         }
+      }
     }
+  }
 }
-var targetNode = document.getElementsByClassName('webchat-root')[0];
-console.log(targetNode);
+var targetNode = document;
+
 var config = {
-    attributes: false,
-    childList: true,
-    subtree: false
+  attributes: false,
+  childList: true,
+  subtree: true
 };
 
 var observer = new MutationObserver(callback);
