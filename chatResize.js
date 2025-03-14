dc2e9284ff61a43ae2aaa81aae6b204a8832bfc2
pmanrezai <160758790+pmanrezai@users.noreply.github.com>
2025-03-14 14:46:39 -0400

    Update chatResize.js

--- chatResize.js
+++ chatResize.js
@@ -19,8 +19,8 @@ var callback = function (mutationsList) {
           if (wcState == "small") {
             wcObject.style.bottom = "0px";
             wcObject.style.right = "0px";
-            wcObject.style.height = "50%";
-            wcObject.style.width = "50%";
+            wcObject.style.height = "100%";
+            wcObject.style.width = "100%";
             b.innerText = "Shrink chat window"
             wcState = "big"
           } else if (wcState === "big") {
